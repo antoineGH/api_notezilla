b0aa315e14c075a0a582f2b9be9783d6a7aab284
Templars <antoine.ratat@gmail.com>
2021-10-19 18:43:41 +0800

    update to heroku psql db

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL')
     # SECRET_KEY = config.get('SECRET_KEY')
     # JWT_SECRET_KEY = config.get('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_DATABASE')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=7)
