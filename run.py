4fe926472fde4048ce21f69fa978c66c9fb29ed6
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-07 21:23:00 +0800

    update debug mode

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
     # gunicorn_logger = logging.getLogger('gunicorn.error')
     # app.logger.handlers = gunicorn_logger.handlers
     # app.logger.setLevel(gunicorn_logger.level)
