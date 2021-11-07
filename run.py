63fedde77fbe4e2e8c48ae127feb8475bfcaae57
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-07 21:14:33 +0800

    heroku psql db

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     # gunicorn_logger = logging.getLogger('gunicorn.error')
     # app.logger.handlers = gunicorn_logger.handlers
     # app.logger.setLevel(gunicorn_logger.level)
